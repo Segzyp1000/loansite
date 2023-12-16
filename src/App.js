675b5a53bf265c12a8a9d3443906ab1935e486d3
segun olowoyeye <segunolowoyeye@gmail.com>
2023-12-16 07:43:48 +0100

    added

--- src/App.js
+++ src/App.js
@@ -5,22 +5,39 @@ import MainPage from './MainPage';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const handleLogin = (data) => {
-    setIsLoading(true); // Set loading to true while transitioning
+    setIsLoading(true);
 
     // Simulating API call or any asynchronous operation
     setTimeout(() => {
-      setIsLoggedIn(data);
-      setIsLoading(false); // Set loading to false after transition
+      // Simulate bad network by setting a condition where the network is down
+      const isNetworkDown = Math.random() < 0.5; // 50% chance of network failure
+
+      if (isNetworkDown) {
+        setIsError(true); // Set error state to true for network failure
+        setIsLoading(false);
+      } else {
+        setIsLoggedIn(true);
+        setIsLoading(false);
+      }
     }, 2000); // Simulating a 2-second delay
   };
 
   return (
     <div className="App">
-      {!isLoggedIn && !isLoading && <Form setIsLoggedIn={handleLogin} />}
-      {isLoggedIn && !isLoading && <MainPage />}
-      {isLoading && <p>Loading...</p>}
+      {!isLoggedIn && !isLoading && !isError && (
+        <Form setIsLoggedIn={handleLogin} />
+      )}
+      {isLoggedIn && !isLoading && !isError && <MainPage />}
+      {isLoading && !isError && <p>Loading...</p>}
+      {isError && (
+        <div className='err'>
+          <p>Page not found due to bad network</p>
+          <a href='https://segzyp1000.github.io/loansite/'>Return to the main Page</a>
+        </div>
+      )}
     </div>
   );
 }
