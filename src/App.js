d18ee69a9ce185aaca414f2d45ddd04132b364f4
segun olowoyeye <segunolowoyeye@gmail.com>
2023-12-16 06:59:14 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 import Form from './Form';
 import MainPage from './MainPage';
-import './App.css';
 
-const App = () => {
+function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = (data) => {
+    setIsLoading(true); // Set loading to true while transitioning
+
+    // Simulating API call or any asynchronous operation
+    setTimeout(() => {
+      setIsLoggedIn(data);
+      setIsLoading(false); // Set loading to false after transition
+    }, 2000); // Simulating a 2-second delay
+  };
 
   return (
-    <div className='App'>
-      {isLoggedIn ? (
-        <MainPage />
-      ) : (
-        <Form setIsLoggedIn={setIsLoggedIn} />
-      )}
+    <div className="App">
+      {!isLoggedIn && !isLoading && <Form setIsLoggedIn={handleLogin} />}
+      {isLoggedIn && !isLoading && <MainPage />}
+      {isLoading && <p>Loading...</p>}
     </div>
   );
-};
+}
 
 export default App;
